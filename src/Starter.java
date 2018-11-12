c9cd06f832e610249a40f7d663c2e577c05247f3
Mariusz Siedlecki <malutkis@poczta.fm>
2018-11-12 10:42:42 +0100

    Init comit

---
+++ src/Starter.java
@@ -0,0 +1,12 @@
+/**
+ * Mariusz Siedlecki
+ */
+public class Starter {
+    public static void main(String[] args) {
+        System.out.println("welcome to the git");
+
+        public static void printsomeText(){
+            System.out.println("coś");
+        }
+    }
+}
